bd992f96bd09c2c45dd97c617f693c7b30fd6d5d
dastanaron <flow199@yandex.ru>
2025-07-03 23:22:45 +0300

    Updated onInit

--- lua/prisma/lsp.lua
+++ lua/prisma/lsp.lua
@@ -26,9 +26,16 @@ function M.setup(config)
         end
     end
 
+    local default_on_init = function(client, bufnr)
+        if config.lsp.on_init then
+            config.lsp.on_init(client, bufnr)
+        end
+     end
+
     lspconfig.prismals.setup(vim.tbl_deep_extend("force", {
         filetypes = { 'prisma' },
         on_attach = default_on_attach,
+        on_init = default_on_init,
         capabilities = config.lsp.capabilities
     }, config.lsp.settings))
 end
